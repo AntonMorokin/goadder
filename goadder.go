e9ba45c5209f3b5125e9b30264f3ecb38bb5b7f3
Anton Morokin <anton.morokin@gmail.com>
2025-09-27 17:57:50 +0300

    Added godoc

--- goadder.go
+++ goadder.go
@@ -1,5 +1,19 @@
+// Package goadder provides simple integer addition functionality.
 package goadder
 
+// Add returns the sum of two integers.
+//
+// Parameters:
+//   a - the first integer to add
+//   b - the second integer to add
+//
+// Returns:
+//   The sum of a and b
+//
+// Example:
+//   sum := Add(5, 3) // sum == 8
+//   sum := Add(-2, 7) // sum == 5
+// See [funny link]: https://mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
