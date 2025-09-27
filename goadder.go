3d168956348f5b8bd9670b37a8636e4ceef4c27b
Anton Morokin <anton.morokin@gmail.com>
2025-09-27 18:01:38 +0300

    Fixed godoc

--- goadder.go
+++ goadder.go
@@ -13,7 +13,8 @@ package goadder
 // Example:
 //   sum := Add(5, 3) // sum == 8
 //   sum := Add(-2, 7) // sum == 5
-// See [funny link]: https://mathsisfun.com/numbers/addition.html
+// See [funny link]
+// [funny link]: https://mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
